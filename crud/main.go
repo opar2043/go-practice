21e67b4e7c49602d10c894dcda7d578415960130
Rijoan Rashid Opar <rezonerashidopar@gmail.com>
2026-06-03 10:44:27 -0700

    get methods added

--- crud/main.go
+++ crud/main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type User struct {
+	Id int
+	Name string
+	Age  int
+
+}
+
+var users = []User{
+	{
+		Id: 1,
+		Name: "John Doe",
+		Age: 30,
+	},
+	{
+		Id: 2,
+		Name: "Jane Smith",
+		Age: 25,
+	},
+}
+
 func main(){
-	http.HandleFunc("/" , rootHandler )
+	mux := http.NewServeMux();
+	
+	mux.HandleFunc("/" , rootHandler )
+	mux.HandleFunc("/health" , healthHandler )
+	mux.HandleFunc("POST /create" , createHandler )
+	mux.HandleFunc("GET /users" , getUsersHandler )
+
 	fmt.Println("Server is running port 5000")
-	err := http.ListenAndServe(":5000", nil)
+	err := http.ListenAndServe(":5000", mux)
 
 	if err != nil {
 		fmt.Println("Server error: ", err);
@@ -16,6 +43,28 @@ func main(){
 
 }
 
+
+
 func rootHandler(w http.ResponseWriter, r *http.Request){
  fmt.Fprintln(w ,"Welcome to go server")
+}
+func healthHandler(w http.ResponseWriter, r *http.Request){
+ fmt.Fprintln(w ,"Health check passed")
+}
+func createHandler(w http.ResponseWriter, r *http.Request){
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		// w.WriteHeader(405)
+		fmt.Fprintln(w , "Methode not allowed");
+		return
+	}
+
+   fmt.Fprintln(w ,"Create endpoint")
+}
+func getUsersHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-type", "application/json")
+	users,_ := json.Marshal(users)
+	w.Write(users)
+
+   fmt.Fprintln(w ,"Get Users endpoint")
 }
