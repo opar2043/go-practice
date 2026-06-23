4fb2fb215311405f6233075c5e740ef0b6e3408a
Rijoan Rashid Opar <rezonerashidopar@gmail.com>
2026-06-23 09:47:25 -0700

    student manage route completed

--- student-manage/main.go
+++ student-manage/main.go
@@ -1,9 +1,9 @@
-package studentmanage
+package main
 import (
 	"strconv"
-
 	"github.com/gofiber/fiber/v2"
 )
+
 type Student struct {
 	ID         int     `json:"id"`
 	Name       string  `json:"name"`
@@ -20,14 +20,13 @@ type Payment struct {
 }
 
 var students []Student
-var Payments []Payment
+var payments []Payment
 var err error;
 
 func main(){
 	app := fiber.New();
 
-	
-		// routes
+	// routes
 	app.Get("/students", getStudents)
 	app.Post("/students", createStudent)
 	app.Get("/students/:id", getStudent)
@@ -40,7 +39,12 @@ func main(){
 	app.Put("/payments/:id", updatePayment)
 	app.Delete("/payments/:id", deletePayment)
 
+	app.Get("/", func(c *fiber.Ctx) error {
+	return c.SendString("Student Management API is running!")
+    })
+
 	app.Listen(":3000")
+	
 }
 
 func getStudents(c *fiber.Ctx) error {
@@ -61,7 +65,7 @@ func createStudent(c *fiber.Ctx) error{
 
 func updateStudent(c *fiber.Ctx) error{
   id , _ := strconv.Atoi(c.Params("id"))
-
+  
   var body Student
   if err != nil {
 	return  c.Status(400).JSON("Error found")
@@ -76,4 +80,85 @@ func updateStudent(c *fiber.Ctx) error{
   }
 
   return c.Status(401).JSON(fiber.Map{"message": "Not found"})
+}
+
+func getStudent(c *fiber.Ctx) error {
+	id,_ := strconv.Atoi(c.Params("id"))
+
+	for _,s := range students {
+		if s.ID ==id {
+			return  c.JSON(s)
+		}
+	}
+
+	return c.Status(404).JSON(fiber.Map{"message": "Student not found"})
+}
+
+func deleteStudent(c *fiber.Ctx) error {
+	id , _ := strconv.Atoi(c.Params("id"))
+
+	for i,s := range students {
+		if s.ID == id {
+			students = append(students[:i] , students[i+1:]...)
+			return c.Status(200).JSON(fiber.Map{"message": "Student deleted successfully"})
+		}
+	}
+	return c.Status(404).JSON(fiber.Map{"message": "Student not found"})
+}
+
+// payments route 
+func getPayments (c *fiber.Ctx) error {
+  return c.JSON(payments)
+}
+
+func createPayment(c *fiber.Ctx) error {
+	var p Payment 
+    if err := c.BodyParser(&p); 
+
+    err != nil {
+		return c.Status(400).JSON("Invalid request body Wehen creating User")
+	}
+
+	payments = append(payments, p)
+	return  c.Status(200).JSON(p)
+}
+
+func getPayment (c *fiber.Ctx) error {
+	id , _ := strconv.Atoi(c.Params("id"))
+	for _,p := range payments {
+		if p.ID == id {
+			return  c.JSON(p)
+		}
+
+	}
+	return c.Status(404).JSON(fiber.Map{"message": "Payment not found"})
+}
+
+func updatePayment (c *fiber.Ctx)error {
+  id,_ := strconv.Atoi(c.Params("id"))
+  var body Payment
+
+  if err != nil {
+	return  c.Status(400).JSON("Error found")
+  }
+
+  for i,p := range payments {
+	if p.ID == id {
+		body.ID = p.ID
+		payments[i] = body
+		return c.JSON(body)
+	}
+  }
+
+  return c.Status(404).JSON(fiber.Map{"message": "Payment not found"});
+}
+
+func deletePayment(c *fiber.Ctx) error {
+	id,_ := strconv.Atoi(c.Params("id"))
+	for i,p := range payments {
+		if p.ID == id {
+			payments = append(payments[:i], payments[i+1:]... )
+		}
+	}
+	return c.Status(200).JSON(fiber.Map{"message": "Payment deleted successfully"})
 }
